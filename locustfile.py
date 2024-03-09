0d4c2ff980bd91e02e4a3ba7d7d1d3838ae37438
Taylor520180 <154802853+Taylor520180@users.noreply.github.com>
2024-03-09 15:56:34 +0800

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -45,17 +45,22 @@ class ChatUser(HttpUser):
                 "messages": [
                     {"content": "What is Mosaic Theory?", "role": "user"},
                     {
-                        "content": "Based on Mosaic Theory, a financial analyst may use significant conclusions derived from the analysis of public and nonmaterial nonpublic information as the basis for investment recommendations and decisions even if those conclusions would have been material inside information had they been communicated directly to the analyst by a company. Under the Mosaic Theory, financial analysts are free to act on this collection, or mosaic, of information without risking violation.",
+                        "content": "Good question! Mosaic Theory refers to .... In summary, Mosaic Theory is about...",
                         "role": "assistant",
                     },
-                    {"content": "Can you show me case examples related to Mosaic Theory?", "role": "user"},
+                    {"content": "Can you show me an example related to Mosaic Theory?", "role": "user"},
                     {
-                        "content": "Case Exmaple: Roger Clement is a senior financial analyst who specializes in the European automobile sector at Rivoli Capital.After speaking with representatives of Turgot Chariots—a European auto manufacturer with sales primarily in South Korea—and after conducting interviews with salespeople, labor leaders, his firm’s Korean currency analysts, and banking officials, Clement thinks the prospect of Turgot Chariots is not promising. He then changes his conclusion about the company from “market outperform” to “market underperform.” Clement retains the support material used to reach his conclusion in case questions later arise. Comment-To reach a conclusion about the value of the company, Clement has pieced together a number of nonmaterial or public bits of information that affect Turgot Chariots. Therefore, under the mosaic theory, Clement has not violated Standard II(A) in drafting the report.",
+                        "content": "Of course! Let's see this example:\n\nRoger Clement is a senior financial analyst who specializes in the European automobile sector at Rivoli Capital....\n\nAnalysis: to reach a conclusion about the value of the company, Clement has pieced together a number of nonmaterial or public bits of information that affect Turgot Chariots. Therefore, under the mosaic theory, Clement has not violated Standard II(A) in drafting the report.",
                         "role": "assistant",
                     },
-                    {"content": "Can you provide me with a quiz question related to Ethical Standard II(A) - Material Nonpublic Information?", "role": "user"},
+                    {"content": "Can you provide me with a quiz related to Ethical Standard II(A) - Material Nonpublic Information?", "role": "user"},
                     {
-                        "content": "Winifred is an investment analyst who has accumulated and analyzed several pieces of nonpublic information through her contacts with drug firms. Although none of the information is"material," Winifred correctly concluded that the earnings of one of the drug firms would be unexpectedly high in the coming year. According to the Code and Standards, Winifred: A. can use the information to make investment recommendations and decisions. B. should publish the information before she uses it in her investment recommendations and decisions. C. should urge the drug firm to publicly disseminate the information immediately. Which one is the correct answer?",
+                        "content": "Sure! Ready for the challenge?\n\nUnder the Code and Standards, which of the following investment risks is least likely required to be disclosed to clients?\nAnswer Choices:\nA. the use of leverage as part of the investment strategy.\nB. risks that are unknown to the investment adviser at the time of the recommendation.\nC.general market-related risks.\"\n\nWhich is the correct answer?"",
+                        "role": "assistant",
+                    },
+                    {"content": "I still don't understand.", "role": "user"},
+                    {
+                        "content": "No worries! Let me give you a more concrete example to make it clearer. Imaging. you are .... Does it sound clear to you now? Free to ask if you have more questions.",
                         "role": "assistant",
                     },
                 ],
