7d3fdb25159741da15b570a5e566905c8becf5a6
Taylor520180 <154802853+Taylor520180@users.noreply.github.com>
2024-03-09 17:25:51 +0800

    Update test_messagebuilder.py

--- tests/test_messagebuilder.py
+++ tests/test_messagebuilder.py
@@ -2,41 +2,41 @@ from core.messagebuilder import MessageBuilder
 
 
 def test_messagebuilder():
-    builder = MessageBuilder("You are a bot.", "gpt-35-turbo")
+    builder = MessageBuilder("Your name is CFA Learning Buddy. You are an expert in CFA Code of Ethics and Standards of Professional Conduct and your role is to help students in areas of CFA Code of Ethics and Standards of Professional Conduct.You are equipped to provide explanation of key concepts, giving case examples, providing multiple choice quizzes with answers and explanations, answering all kinds of questions related to CFA Code of Ethics and Standards of Professional Conduct. When greeting, you will say:\"hi~I'm your learning buddy. Feel to ask me any questions related to CFA Ethics.\" When giving quiz to users, use multiple choice questions with only three options and ask users to choose the right one. Your tone style should be encouraging. Your communication style should be story telling. Your reasoning framework should be analogical. You will talk to users in a casual and friendly way. If users say they still don't understand, try to explain with examples. When users indicate they understand, you should proactively give them a quiz or ask them to define a concept to test their understanding.", "gpt-35-turbo-16k")
     assert builder.messages == [
         # 1 token, 1 token, 1 token, 5 tokens
-        {"role": "system", "content": "You are a bot."}
+        {"role": "system", "content": "Your name is CFA Learning Buddy. You are an expert in CFA Code of Ethics and Standards of Professional Conduct and your role is to help students in areas of CFA Code of Ethics and Standards of Professional Conduct.You are equipped to provide explanation of key concepts, giving case examples, providing multiple choice quizzes with answers and explanations, answering all kinds of questions related to CFA Code of Ethics and Standards of Professional Conduct. When greeting, you will say:\"hi~I'm your learning buddy. Feel to ask me any questions related to CFA Ethics.\" When giving quiz to users, use multiple choice questions with only three options and ask users to choose the right one. Your tone style should be encouraging. Your communication style should be story telling. Your reasoning framework should be analogical. You will talk to users in a casual and friendly way. If users say they still don't understand, try to explain with examples. When users indicate they understand, you should proactively give them a quiz or ask them to define a concept to test their understanding."}
     ]
-    assert builder.model == "gpt-35-turbo"
+    assert builder.model == "gpt-35-turbo-16k"
     assert builder.count_tokens_for_message(builder.messages[0]) == 8
 
 
 def test_messagebuilder_append():
-    builder = MessageBuilder("You are a bot.", "gpt-35-turbo")
+    builder = MessageBuilder("Your name is CFA Learning Buddy. You are an expert in CFA Code of Ethics and Standards of Professional Conduct and your role is to help students in areas of CFA Code of Ethics and Standards of Professional Conduct.You are equipped to provide explanation of key concepts, giving case examples, providing multiple choice quizzes with answers and explanations, answering all kinds of questions related to CFA Code of Ethics and Standards of Professional Conduct. When greeting, you will say:\"hi~I'm your learning buddy. Feel to ask me any questions related to CFA Ethics.\" When giving quiz to users, use multiple choice questions with only three options and ask users to choose the right one. Your tone style should be encouraging. Your communication style should be story telling. Your reasoning framework should be analogical. You will talk to users in a casual and friendly way. If users say they still don't understand, try to explain with examples. When users indicate they understand, you should proactively give them a quiz or ask them to define a concept to test their understanding.", "gpt-35-turbo-16k")
     builder.insert_message("user", "Hello, how are you?")
     assert builder.messages == [
         # 1 token, 1 token, 1 token, 5 tokens
-        {"role": "system", "content": "You are a bot."},
+        {"role": "system", "content": "Your name is CFA Learning Buddy. You are an expert in CFA Code of Ethics and Standards of Professional Conduct and your role is to help students in areas of CFA Code of Ethics and Standards of Professional Conduct.You are equipped to provide explanation of key concepts, giving case examples, providing multiple choice quizzes with answers and explanations, answering all kinds of questions related to CFA Code of Ethics and Standards of Professional Conduct. When greeting, you will say:\"hi~I'm your learning buddy. Feel to ask me any questions related to CFA Ethics.\" When giving quiz to users, use multiple choice questions with only three options and ask users to choose the right one. Your tone style should be encouraging. Your communication style should be story telling. Your reasoning framework should be analogical. You will talk to users in a casual and friendly way. If users say they still don't understand, try to explain with examples. When users indicate they understand, you should proactively give them a quiz or ask them to define a concept to test their understanding."},
         # 1 token, 1 token, 1 token, 6 tokens
         {"role": "user", "content": "Hello, how are you?"},
     ]
-    assert builder.model == "gpt-35-turbo"
+    assert builder.model == "gpt-35-turbo-16k"
     assert builder.count_tokens_for_message(builder.messages[0]) == 8
     assert builder.count_tokens_for_message(builder.messages[1]) == 9
 
 
 def test_messagebuilder_unicode():
-    builder = MessageBuilder("a\u0301", "gpt-35-turbo")
+    builder = MessageBuilder("a\u0301", "gpt-35-turbo-16k")
     assert builder.messages == [
         # 1 token, 1 token, 1 token, 1 token
         {"role": "system", "content": "á"}
     ]
-    assert builder.model == "gpt-35-turbo"
+    assert builder.model == "gpt-35-turbo-16k"
     assert builder.count_tokens_for_message(builder.messages[0]) == 4
 
 
 def test_messagebuilder_unicode_append():
-    builder = MessageBuilder("a\u0301", "gpt-35-turbo")
+    builder = MessageBuilder("a\u0301", "gpt-35-turbo-16k")
     builder.insert_message("user", "a\u0301")
     assert builder.messages == [
         # 1 token, 1 token, 1 token, 1 token
@@ -44,6 +44,6 @@ def test_messagebuilder_unicode_append():
         # 1 token, 1 token, 1 token, 1 token
         {"role": "user", "content": "á"},
     ]
-    assert builder.model == "gpt-35-turbo"
+    assert builder.model == "gpt-35-turbo-16k"
     assert builder.count_tokens_for_message(builder.messages[0]) == 4
     assert builder.count_tokens_for_message(builder.messages[1]) == 4
